a07d1b7234ac0270fa52758ffbe226e7ceec0c79
miyabiit <yamada.masahiro@gmail.com>
2013-06-23 15:37:15 +0900

    update

--- web.js
+++ web.js
@@ -3,6 +3,5 @@ var express = require('express');
 var app = express();
 app.configure(function(){
 	app.use(express.static(__dirname + '/'));
-	//app.use(express.static(__dirname + '/hackernews/'));
 });
 app.listen(3000);
